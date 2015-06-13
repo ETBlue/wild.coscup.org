2612a76bc15e7317c7ef9002a54a40b2ca6d9b46
Yong Qian, Lee <b92101007@gmail.com>
2015-06-13 15:34:20 +0800

    建立基本設定檔

---
+++ public/js/config.js
@@ -0,0 +1,18 @@
+CONFIG = {
+  //寫在google spread sheets與網頁使用的時間格式
+  timeformat: 'YYYY-MM-DD HH:mm:ss',
+  //寫在google spread sheets與網頁使用的時區
+  timezone: '+08:00',
+  //schedule設定
+  schedule: {
+    session: {
+      url: '1FtOxGJRvi-8iRRPEFfgXGASvS5RjDWh0tcVPBmoU0JA'
+    },
+    workshop: {
+      url: '1FtOxGJRvi-8iRRPEFfgXGASvS5RjDWh0tcVPBmoU0JA'
+    },
+    unconf: {
+      url: '1FtOxGJRvi-8iRRPEFfgXGASvS5RjDWh0tcVPBmoU0JA'
+    }
+  }
+};
